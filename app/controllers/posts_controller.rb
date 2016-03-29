0f3c545b29da051c97b091b5fecd1891227d3148
Joshua Munene <kenyadevelop@gmail.com>
2016-03-29 08:28:04 -0700

    Add association between models

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -6,7 +6,7 @@ class PostsController < ApplicationController
 	end
 
 	def new
-		@post = Post.new
+		@post = current_user.posts.build
 	end
 
 	def show
@@ -14,7 +14,7 @@ class PostsController < ApplicationController
 	end
 
 	def create
-		@post = Post.new(post_params)
+		@post = current_user.posts.build(post_params)
 		if @post.save
 			redirect_to @post
 		else
